e67da0f6379e9b6b6f5831ccb7c8915c9f702a21
Vincent Martinez <eykosioux@gmail.com>
2016-05-23 11:16:42 +0100

    Add example integration test (GET /spaces)

---
+++ test/integration/spaces_api_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class SpacesApiTest < ActionDispatch::IntegrationTest
+
+  # example test case for GET /spaces
+  describe "get /spaces" do
+
+    it "returns a list of spaces" do
+      get "/spaces"
+      response.success?.must_equal true
+
+      list = Space.all
+      resp = JSON.parse body
+
+      resp["spaces"].count.must_equal 2
+    end
+
+    it "serializes each space with ActiveModel::Serializer" do
+      space = Space.first
+      serializer = SpaceSerializer.new(space)
+      adapter = ActiveModelSerializers::Adapter.create(serializer)
+
+      get "/spaces/#{space.id}"
+
+      adapter.to_json.must_equal body
+    end
+  end
+end
