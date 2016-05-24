e4f8ecbcae80c7c25cf210d1c97d09e3fca85b08
Vincent Martinez <eykosioux@gmail.com>
2016-05-24 15:04:01 +0100

    Cleanup

--- app/models/amenity.rb
+++ app/models/amenity.rb
@@ -10,6 +10,6 @@
 
 class Amenity < ApplicationRecord
   has_many :space_amenities, dependent: :destroy
-  has_many :spaces, through: :space_amenities, dependent: :destroy
+  has_many :spaces, through: :space_amenities
   validates :name, uniqueness: true, presence: true
 end
--- app/models/space_type.rb
+++ app/models/space_type.rb
@@ -9,6 +9,16 @@
 #
 
 class SpaceType < ApplicationRecord
+  before_destroy :check_for_spaces
   has_many :spaces
   validates :name, uniqueness: true, presence: true
+
+  private
+
+  def check_for_spaces
+    if spaces.any?
+      errors.add_to_base('cannot delete space types with associated spaces')
+      return false
+    end
+  end
 end
