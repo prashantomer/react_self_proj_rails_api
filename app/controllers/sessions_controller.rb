50b555f92df70f99b302b0f06c005e917532ac6e
Shiv Patel <shiv.yuvasoft@gmail.com>
2016-05-25 16:32:56 +0530

    Feature # Return user with token after successfull sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,14 @@
 class SessionsController < Devise::SessionsController
-  # login user / password *not* oauth
+  def create
+    credentials = params[:credentials]
+    user = User.find_by_email(credentials[:email])
+
+    if user && user.valid_password?(credentials[:password])
+      render json: { user: user,
+                     token: AuthToken.encode(user.token_payload) },
+             status: :ok
+    else
+      return head(:unauthorized)
+    end
+  end
 end
