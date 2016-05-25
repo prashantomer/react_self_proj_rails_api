62b3b95d88215ca78c7d19644002545ef25019b5
Shiv Patel <shiv.yuvasoft@gmail.com>
2016-05-25 19:30:15 +0530

    Feature # Space create method specs

--- test/integration/spaces_api_test.rb
+++ test/integration/spaces_api_test.rb
@@ -1,6 +1,13 @@
 require 'test_helper'
 
 class SpacesApiTest < ActionDispatch::IntegrationTest
+  before(:each) do
+    @user_headers = { CONTENT_TYPE: 'application/json',
+                      AUTHORIZATION: token_for(users(:normal)) }
+    @admin_headers = { CONTENT_TYPE: 'application/json',
+                       AUTHORIZATION: token_for(users(:admin)) }
+  end
+
   # example test case for GET /spaces
   describe 'get /spaces' do
     it 'returns a list of spaces' do
@@ -17,12 +24,35 @@ class SpacesApiTest < ActionDispatch::IntegrationTest
       space = Space.first
       serializer = SpaceSerializer.new(space)
       adapter = ActiveModelSerializers::Adapter.create(serializer)
-      headers = { CONTENT_TYPE: 'application/json',
-                  AUTHORIZATION: token_for(users(:normal)) }
 
-      get "/spaces/#{space.id}", headers: headers
+      get "/spaces/#{space.id}", headers: @user_headers
 
       adapter.to_json.must_equal body
+    end
+  end
+
+  describe 'create' do
+    before(:each) do
+      space_type_id = SpaceType.create(name: Faker::Name.name).id
+      @params = { space:
+                  { space_type_id: space_type_id,
+                    title: Faker::Name.title,
+                    address_line_1: Faker::Address.street_address,
+                    city: Faker::Address.city,
+                    country: Faker::Address.country,
+                    latitude: Faker::Address.latitude,
+                    longitude: Faker::Address.longitude } }.to_json
+    end
+    it 'responds with 201 when user is normal user' do
+      post spaces_path, params: @params, headers: @user_headers
+
+      response.status.must_equal 201
+    end
+
+    it 'responds with 201 when user is an admin' do
+      post spaces_path, params: @params, headers: @admin_headers
+
+      response.status.must_equal 201
     end
   end
 end
