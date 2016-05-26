feb8aa7a18602ac5ebbf64341cd3067a1c349798
Shiv Patel <shiv.yuvasoft@gmail.com>
2016-05-26 15:35:46 +0530

    Feature # writing spaces integration specs

--- test/controllers/spaces_controller_test.rb
+++ test/controllers/spaces_controller_test.rb
@@ -2,8 +2,51 @@ require 'test_helper'
 
 class SpacesControllerTest < ActionDispatch::IntegrationTest
   # TODO: make useful tests
-  test 'index should not be restricted' do
-    get spaces_url
-    assert_response :ok
+
+  setup do
+    @space = spaces(:one)
+    @admin = users(:admin)
+    @user = users(:normal)
   end
+
+  test 'index should not be restricted' do
+    get spaces_path
+    assert_response :success
+  end
+
+  test 'show should be restricted' do
+    get space_path(@space)
+    assert_response 401
+  end
+
+  test 'show should be access by any user' do
+    get space_path(@space), headers: { Authorization: token_for(@user) }
+    assert_response :success
+  end
+
+  # test 'should create spaces' do
+  #   assert_difference('Space.count') do
+  #     post spaces_path,
+  #          params: { space: { name: Faker::Name.name } },
+  #          headers: { Authorization: token_for(@current_user) }
+  #   end
+
+  #   assert_response 201
+  # end
+
+  # test 'should update space' do
+  #   patch space_path(@space),
+  #         params: { space: { name: Faker::Name.name } },
+  #         headers: { Authorization: token_for(@current_user) }
+  #   assert_response 200
+  # end
+
+  # test 'should destroy space' do
+  #   assert_difference('space.count', -1) do
+  #     delete space_path(@space),
+  #            headers: { Authorization: token_for(@current_user) }
+  #   end
+
+  #   assert_response :ok
+  # end
 end
--- test/integration/spaces_api_test.rb
+++ test/integration/spaces_api_test.rb
@@ -1,15 +1,17 @@
 require 'test_helper'
 
 class SpacesApiTest < ActionDispatch::IntegrationTest
-  before(:each) do
+  setup do
+    @user = users(:normal)
+    @admin = users(:admin)
     @user_headers = { CONTENT_TYPE: 'application/json',
-                      AUTHORIZATION: token_for(users(:normal)) }
+                      AUTHORIZATION: token_for(@user) }
     @admin_headers = { CONTENT_TYPE: 'application/json',
-                       AUTHORIZATION: token_for(users(:admin)) }
+                       AUTHORIZATION: token_for(@admin) }
   end
 
   # example test case for GET /spaces
-  describe 'get /spaces' do
+  describe 'index' do
     it 'returns a list of spaces' do
       get '/spaces'
       response.success?.must_equal true
@@ -18,6 +20,14 @@ class SpacesApiTest < ActionDispatch::IntegrationTest
       resp = JSON.parse body
 
       resp['spaces'].count.must_equal list.count
+    end
+  end
+
+  describe 'show' do
+    it 'should be restricted' do
+      space = Space.first
+      get "/spaces/#{space.id}"
+      response.status.must_equal 401
     end
 
     it 'serializes each space with ActiveModel::Serializer' do
@@ -32,7 +42,7 @@ class SpacesApiTest < ActionDispatch::IntegrationTest
   end
 
   describe 'create' do
-    before(:each) do
+    setup do
       space_type_id = SpaceType.create(name: Faker::Name.name).id
       @params = { space:
                   { space_type_id: space_type_id,
@@ -43,6 +53,12 @@ class SpacesApiTest < ActionDispatch::IntegrationTest
                     latitude: Faker::Address.latitude,
                     longitude: Faker::Address.longitude } }.to_json
     end
+
+    it 'should be restricted' do
+      post spaces_path(@space), params: @params
+      response.status.must_equal 401
+    end
+
     it 'responds with 201 when user is normal user' do
       post spaces_path, params: @params, headers: @user_headers
 
@@ -53,6 +69,48 @@ class SpacesApiTest < ActionDispatch::IntegrationTest
       post spaces_path, params: @params, headers: @admin_headers
 
       response.status.must_equal 201
+    end
+  end
+
+  describe 'update' do
+    setup do
+      @space_type_id = SpaceType.create(name: Faker::Name.name).id
+      @params = { space:
+                  { space_type_id: @space_type_id,
+                    title: Faker::Name.title,
+                    address_line_1: Faker::Address.street_address,
+                    city: Faker::Address.city,
+                    country: Faker::Address.country,
+                    latitude: Faker::Address.latitude,
+                    longitude: Faker::Address.longitude } }.to_json
+      @space = spaces(:one)
+    end
+
+    it 'should be restricted' do
+      put space_path(@space), params: @params
+      response.status.must_equal 401
+    end
+
+    it 'responds with 200 OK when user is an administrator' do
+      put space_path(@space), params: @params, headers: @user_headers
+      response.status.must_equal 200
+    end
+  end
+
+  describe 'delete' do
+    setup do
+      @space = spaces(:one)
+    end
+    it 'should be restricted' do
+      delete space_path(@space)
+      response.status.must_equal 401
+    end
+
+    it 'responds with 200 OK when user is an administrator' do
+      delete space_path(@space),
+             headers: @admin_headers
+
+      response.status.must_equal 200
     end
   end
 end
